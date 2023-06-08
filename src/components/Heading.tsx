ffb2519ddf1ea2328941b6f28d5619a4683961de
alexandre-mace <helloalexandremace@protonmail.com>
2023-06-08 10:31:08 +0200

    Update logo

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,19 @@
 const Heading = () => (
   <section className="relative mx-auto mt-5 max-w-5xl px-4 md:mt-0">
-    <h1 className="text-center text-3xl font-extrabold tracking-tight text-slate-900">
-      Reduce 🪶
-    </h1>
-
+    <div className={"flex items-center justify-center gap-2"}>
+      <h1 className="text-center text-3xl font-extrabold tracking-tight text-slate-900">
+        Reduce
+      </h1>{" "}
+      <div>
+        <img
+          src="https://em-content.zobj.net/thumbs/240/apple/354/feather_1fab6.png"
+          height={20}
+          width={30}
+          className={"inline-block"}
+          alt="logo plume"
+        />
+      </div>
+    </div>
     <p className="text-md mx-auto mt-2 max-w-3xl text-center text-slate-600">
       Comprenez quels indicateurs ont le plus d&apos;impact sur votre empreinte
     </p>
